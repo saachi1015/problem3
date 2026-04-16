1f73eafcf4acba8756b1f107732de9d731f73983
saachi1015 <saachi1015@gmail.com>
2026-04-16 15:47:16 +0200

    update 4/16

--- coe2rv.m
+++ coe2rv.m
@@ -4,16 +4,16 @@ p = a*(1-e^2);
 r_pf = p/(1+e*cos(theta)) * [cos(theta); sin(theta); 0];
 v_pf =  sqrt(mu/p) * [-sin(theta); e + cos(theta); 0];
 
-R3_Omega = [cos(Omega) sin(Omega) 0;
-    -sin(Omega) cos(Omega) 0;
+R3_Omega = [cos(Omega) -sin(Omega) 0;
+    sin(Omega) cos(Omega) 0;
     0 0 1];
 
 R1_i = [1 0 0;
-    0 cos(i) sin(i);
-    0 -sin(i) cos(i)];
+    0 cos(i) -sin(i);
+    0 sin(i) cos(i)];
 
-R3_omega = [cos(omega) sin(omega) 0;
-    -sin(omega) cos(omega) 0;
+R3_omega = [cos(omega) -sin(omega) 0;
+    sin(omega) cos(omega) 0;
     0 0 1];
 
 dcm = R3_Omega * R1_i * R3_omega;
--- rv2coe.m
+++ rv2coe.m
@@ -1,19 +1,72 @@
-function [a, e, omega, i, w, theta_ta] = rv2coe(r0,v0)
-% define x and z directions in ECI
-x = [1;0;0];
-z = [0;0;1];
+function [a,e,i,Omega,omega,theta_ta,e_vec,h,n] = rv2coe(r0,v0)
 
-muE = 3.986e5; % constant
-r = norm(r0);
-h = cross(r0, v0); % angular momentum
-e_vec = (cross(v0, h) / muE) - (r0 / r); % eccentricity vector
-e = norm(e_vec); % eccentricity value
-a = dot(h,h) / (muE*(1-e^2)); % semi-major axis
-i = mod(acos(dot(h,z)/norm(h)), 2*pi); % angle of inclination
-n = cross(z,h).';% line of nodes
-omega = mod(acos(dot(n,x)/(norm(x)*norm(n))), 2*pi); % right ascension of the
-% ascending node
-w = mod(acos(dot(n,e_vec)/(norm(n)*e)),2*pi); % argument of perapsis
-theta_ta = mod(acos(dot(e_vec,r0)/(e*r)),2*pi); % true anomaly
+    % constants
+    muE = 3.986e5;  % km^3/s^2
 
+    % basis vectors
+    xhat = [1;0;0];
+    zhat = [0;0;1];
+
+    % make sure inputs are columns
+    r0 = r0(:);
+    v0 = v0(:);
+
+    % magnitudes
+    r = norm(r0);
+    v = norm(v0);
+
+    % angular momentum
+    h = cross(r0, v0);
+    hmag = norm(h);
+
+    % node vector
+    n = cross(zhat, h);
+    nmag = norm(n);
+
+    % eccentricity vector
+    e_vec = (cross(v0, h) / muE) - (r0 / r);
+    e = norm(e_vec);
+
+    % specific orbital energy
+    eps = v^2/2 - muE/r;
+
+    % semi-major axis
+    if abs(e - 1) > 1e-12
+        a = -muE / (2*eps);
+    else
+        a = Inf; % parabolic case
+    end
+
+    % inclination
+    i = acos(h(3)/hmag);
+
+    % RAAN
+    if nmag > 1e-12
+        Omega = acos(n(1)/nmag);
+        if n(2) < 0
+            Omega = 2*pi - Omega;
+        end
+    else
+        Omega = 0; % equatorial orbit
+    end
+
+    % argument of periapsis
+    if nmag > 1e-12 && e > 1e-12
+        omega = acos(dot(n, e_vec)/(nmag*e));
+        if e_vec(3) < 0
+            omega = 2*pi - omega;
+        end
+    else
+        omega = 0; % circular and/or equatorial special case
+    end
+
+    % true anomaly
+    if e > 1e-12
+        theta_ta = acos(dot(e_vec, r0)/(e*r));
+        if dot(r0, v0) < 0
+            theta_ta = 2*pi - theta_ta;
+        end
+    else
+        theta_ta = 0; % circular special case
+    end
 end
